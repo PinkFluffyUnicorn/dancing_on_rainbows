ac38b6aa4711b2ba9adf03aeca538e7002a3cc6f
Gerd Schmidt <g-schmidt-email@gmx.de>
2014-10-09 16:27:47 +0200

    Added simple sfml test

--- Project/Intro 2D Spiel/Intro 2D Spiel/Program.cs
+++ Project/Intro 2D Spiel/Intro 2D Spiel/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SFML.Graphics;
+using SFML.Window;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,8 +12,30 @@ namespace Intro_2D_Spiel
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-            Console.WriteLine("");
+            RenderWindow win = new RenderWindow(new VideoMode(800, 600), "Hallo Fenster");
+            win.Clear();
+
+            win.Closed += win_Closed;
+
+            win.Display();
+            while(win.IsOpen())
+            {
+
+                win.DispatchEvents();
+
+            }
+
         }
+
+        static void win_Closed(object sender, EventArgs e)
+        {
+            RenderWindow win = (RenderWindow)sender;
+            win.Close();
+
+        }
+
+
+
+
     }
 }
